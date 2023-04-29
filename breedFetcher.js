ef3fc16bc52fd0cf9a946540eb74709a25493b74
Nosa Agho <nosaoasis@gmail.com>
2023-04-29 18:35:10 +0100

    Added Test
    Refactored the breedFetcher to be exported and called from the index.js file. Also made test cases.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,23 +1,23 @@
 const request = require("request");
-const catBreed = process.argv[2];
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${catBreed}`
 
-try {
-  request(
-    url,
-    (err, resp, body) => {
+const fetchBreedDescription = (breedName, callback) => {
+  const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
+  try {
+    request(url, (err, resp, body) => {
       if (err) {
-        console.log("Requesting URL does not exist!");
+        return callback(`Failed to request details: ${err}`, null);
       }
       const data = JSON.parse(body);
       const breed = data[0];
       if (breed) {
-        console.log(breed.description);
+        return callback(null, breed.description);
       } else {
-        console.log(`Failed to find breed ${breedName}`);
+        return callback(`Failed to find breed ${breedName}`, null);
       }
-    }
-  );
-} catch (error) {
-  console.log("Sorry, an error occured. Please check your API endpoint.");
-}
+    });
+  } catch (error) {
+    console.log("Sorry, an error occured. Please check your API endpoint.");
+  }
+};
+
+module.exports = { fetchBreedDescription };
